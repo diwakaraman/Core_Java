1a859c7232bfa80c1c6bfd3247c305366727ca18
AMAN DIWAKAR <154485677+diwakaraman@users.noreply.github.com>
2026-02-14 13:08:13 +0530

    Learn encapsulation

---
+++ oops/Encapsulation.java
@@ -0,0 +1,15 @@
+package core;
+
+public class Encapsulation {
+    private int rollnumber;
+
+    public int getRollnumber() {
+        return rollnumber;
+
+    }
+    public void setRollnumber(int rollnumber) {
+        this.rollnumber = rollnumber;
+
+    }
+
+}
