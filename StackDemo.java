592da37d7ceffaa86af06fdd14a7a8b3e3d67bf9
Aman <2023aspire53@gmail.com>
2026-02-14 12:37:20 +0530

    stack demo

---
+++ StackDemo.java
@@ -0,0 +1,17 @@
+package Collections;
+
+import java.util.Stack;
+
+public class StackDemo {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        System.out.println(stack);
+        stack.push(5);
+        System.out.println(stack);
+
+    }
+}
